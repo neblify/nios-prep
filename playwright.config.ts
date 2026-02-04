0f3b999294329f46c85300f5604fd8727352ba78
Deepak Naik <deenaik@gmail.com>
2026-02-04 16:34:35 +0530

    feat: Add E2E test for teacher flow and update Playwright's web server command to `vercel dev`.

--- playwright.config.ts
+++ playwright.config.ts
@@ -51,7 +51,7 @@ export default defineConfig({
 
     /* Run your local dev server before starting the tests */
     webServer: {
-        command: 'npm run dev',
+        command: 'vercel dev',
         url: 'http://localhost:3000',
         reuseExistingServer: !process.env.CI,
     },
